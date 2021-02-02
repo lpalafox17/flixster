0502fbe18babadc207844a34d71ac1fa7b37badb
Luis Palafox <Luispalafox077@gmail.com>
2021-02-02 00:20:31 -0800

    Flixster App

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by luis palafox on 2/1/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+   
+    @IBOutlet var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
